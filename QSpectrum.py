f6838e36738e5f37f14e037cb4b7fe7b6bdd666c
Pawel Sikora <sikor6@gmail.com>
2017-05-29 20:33:25 +0200

    Remove unused parts of code

--- QSpectrum.py
+++ QSpectrum.py
@@ -18,14 +18,14 @@ class Parameters:
 	def __init__(self):
 		self.A0 = 10.0
 		self.g0 = 1.0
-		self.Eg = 1.1828
-		self.T = 300
-		self.gamma = 0.001
-		self.gamma2 = 1.0
+		self.Eg = 1.1828#
+		self.T = 300#
+		self.gamma = 0.001#
+		self.gamma2 = 1.0#
 		self.gamma_schodek=35.0
 		self.gamma_schodek2=5.0
 		self.Ef = 0.5 * self.Eg
-		self.E = np.arange(1.08,1.35,0.001)
+		self.E = np.arange(1.08,1.35,0.001)#
 		self.En = np.array([0.49475 + 0.69223 - self.Eg, 0.54384+0.7062 - self.Eg, 0.60827 + 0.7275 - self.Eg])
 		self.CP = np.array([0.98453, 0.94166, 0.79406])
 			
@@ -197,11 +197,6 @@ class Spectrum_generator:
 		plt.savefig("plot_beta.png")
 		plt.show()
 		
-#Pochodna=diff(E, DOS, 1)
-
-#for i in range(0, len(E)):
-	#Widmo[i]=Calka(E, Widmo1, i)
-
 class GUI:
 
     # This is a callback function. The data arguments are ignored
@@ -231,22 +226,46 @@ class GUI:
 
     def __init__(self):
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-    
+    	self.window.set_size_request(500, 350)	
+	self.entry = gtk.Entry()
+        self.entry.set_text("Hello World")
+	
+	self.hbox = gtk.HButtonBox()
+	self.hbox.set_layout(gtk.BUTTONBOX_SPREAD)
+	self.hbox.set_border_width(5)
+	
+	self.bbox = gtk.VButtonBox()
+	self.bbox.set_layout(gtk.BUTTONBOX_EDGE)
+	self.bbox.set_border_width(5)
+        self.bbox.pack_start(self.entry, True, True, 0)	
+	
+	self.button1 = gtk.Button('Generate graph!')
+	self.bbox.add(self.button1)
+
+	self.button2 = gtk.Button('Save graph')
+	self.bbox.add(self.button2)
+
+	self.button3 = gtk.Button('Compare two graphs')
+	self.bbox.add(self.button3)
+
         self.window.connect("delete_event", self.delete_event)
     
         self.window.connect("destroy", self.destroy)
     
         self.window.set_border_width(30)
     
-        self.button = gtk.Button("Generate graph!")
+        #self.button = gtk.Button("Generate graph!")
     
-        self.button.connect("clicked", self.generate_graph, None)
+        self.button1.connect("clicked", self.generate_graph, None)
     
-        self.window.add(self.button)
+        #self.window.add(self.button)
+        self.window.add(self.bbox)
+        #self.window.add(self.hbox)
+        #self.window.add(self.entry)
     
-        self.button.show()
-    
-        self.window.show()
+        #self.button.show()
+    	self.window.show_all()
+        #self.window.show()
 
     def main(self):
         gtk.main()
@@ -255,69 +274,3 @@ if __name__ == "__main__":
 	
 	gui = GUI()
 	gui.main()
-        
-	#count=len(open('LAMBDA_Zmierzona.txt', 'rU').readlines())
-	#zmierzone_x = open('LAMBDA_Zmierzona.txt', 'rU').readlines()
-	#zmierzone_y = open('Zmierzone1.txt', 'rU').readlines()
-
-	#for i in range(0,count):
-	#    zmierzone_x[i]=float(zmierzone_x[i].strip())
-	#    zmierzone_y[i]=float(zmierzone_y[i].strip())
-	##Wiersz=[]
-	##for i in range(0, 4):
-	#    #Wiersz[i] = linecache.getline('Zmierzone1.txt', i)
-	##Zmierzone = open('Zmierzone1.txt').read()
-	##print len(Zmierzone), Zmierzone[43]
-	##print zmierzone_x
-
-	#BLAD=0.0
-
-	#print len(c.LAMBDA), len(zmierzone_x)
-	#print "Blad wynosi: "
-	#print "kT wynosi (w [meV]): ", k*T*1000.0
-
-	##plt.xlim(E[0],E[len(E)-1])
-	##plt.xlim(LAMBDA[len(LAMBDA)-1],LAMBDA[0])
-
-	##plt.plot(E, DOS, 'r', E, DOS2, 'b', E, Hevisajd, 'g', lw=2)
-	##plt.plot(E, Pik, 'r', E, Normal, 'b', E, Delty, 'g', lw=2)
-	##plt.plot(E, Widmo2/max(Widmo2), 'r', E, Widmo5/max(Widmo5), 'b', lw=2)
-
-	##plt.plot(LAMBDA, Widmo/max(Widmo), 'r', LAMBDA, Widmo5/max(Widmo5), 'b', lw=2)
-
-	##-------- DOBRE WIDMA:--------------------------------------------------------------------------------------------
-	##plt.plot(c.LAMBDA, c.Widmo5/max(c.Widmo5), 'r', c.LAMBDA, c.Widmo/max(c.Widmo), 'b', zmierzone_x, zmierzone_y, 'k.', lw=2)
-	#plt.plot(c.E, c.DOS/max(c.DOS), 'r', c.E, c.DOS2/max(c.DOS2), 'b', c.E, c.Hevisajd/max(c.Hevisajd), 'g', lw=2)
-	##-----------------------------------------------------------------------------------------------------------------
-
-	##plt.plot(LAMBDA, Widmo/max(Widmo), 'r', LAMBDA, Delta(LAMBDA-1.24/(Eg+En[2])), 'b', lw=2)
-	##plt.plot(zmierzone_x[::-1], zmierzone_y, 'r', lw=2)
-
-	##f, axarr = plt.subplots(2, sharex=True)
-	##axarr[0].plot(LAMBDA, Widmo5/max(Widmo5), 'r', zmierzone_x, zmierzone_y, 'b', lw=2)
-	##axarr[0].set_title('Sharing X axis')
-	##if len(LAMBDA) < len(zmierzone_x):
-	#    #axarr[1].plot(LAMBDA, BLAD, 'ro', lw=2)
-	##else:
-	#    #axarr[1].plot(zmierzone_x, BLAD, 'ro', lw=2)
-	##axarr[1].ylim(-1.0,1.0)
-
-	##plt.plot(c.E, co.Ogon_gestosci_stanow(c.E, c.Eg, 1.0, 100.0), 'r', lw=2)
-	##plt.plot(E, Rozklad_Boltzmanna(0.0, E, T), 'r', lw=2)
-	##plt.plot(E, DOS, 'r', E, DOS2, 'b', lw=2)
-	##plt.plot(E, Hevisajd, 'r', E, Delty, 'b', lw=2)
-	##plt.plot(E, Hevisajd*Delty, 'r', lw=2)
-	##plt.plot(E, Prawd/max(Prawd), 'r', E, DOS/max(DOS), 'b', lw=2)
-	##plt.plot(E, Widmo/max(Widmo), 'r', lw=2)
-	##plt.plot(E, Epocz, 'r', lw=2)
-	##plt.plot(E, DOS*Hevisajd, 'r', lw=2)
-	##plt.plot(E, Pik_Lorentza(E, Eg+En[0], gamma)/max(Pik_Lorentza(E, Eg+En[0], gamma)), 'r', E, Pik_Gaussowski(E, Eg+En[0], gamma)/max(Pik_Gaussowski(E, Eg+En[0], gamma)), 'b', lw=2)
-
-	##line_up, = plt.plot(E, Widmo3/max(Widmo3), 'r', label='Pik normalny, DOS normalny', lw=2)
-	##line_down, = plt.plot(E, Widmo4/max(Widmo4), 'b', label='Pik lorentza, DOS lorentza', lw=2)
-	##line_up2, = plt.plot(E, Widmo6/max(Widmo6), 'g', label='Pik normalny, DOS lorentza', lw=2)
-	##line_down2, = plt.plot(E, Widmo7/max(Widmo7), 'y', label='Pik lorentza, DOS normalny', lw=2)
-	##plt.legend([line_up, line_down, line_up2, line_down2], ['Line Up', 'Line Down', 'Line Up2', 'Line Down2'])
-
-	#plt.savefig("pik1.png")
-	#plt.show()
