689fdfd34a6ae6f4dfc9447d022a4a905440b765
Pawel Sikora <sikor6@gmail.com>
2017-05-30 20:12:31 +0200

    Fix indentation from tabs to 4 spaces in gui.py

--- gui.py
+++ gui.py
@@ -8,40 +8,44 @@ class GUI:
 
     def generate_graph(self, widget, data=None):
         c = Spectrum_generator()
-	c.calculate_all()
-	c.plot_widmo_beta()
+        c.calculate_all()
+        c.plot_widmo_beta()
 
     def delete_event(self, widget, event, data=None):
-        print "delete event occurred"
+        print("delete event occurred")
         return False
 
     def destroy(self, widget, data=None):
-        print "destroy signal occurred"
+        print("destroy signal occurred")
         gtk.main_quit()
 
     def __init__(self):
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-    	self.window.set_size_request(500, 350)	
-	self.entry = gtk.Entry()
+        self.window.set_size_request(500, 350)    
+    
+        grid = gtk.Grid()
+        self.window.add(grid)    
+    
+        self.entry = gtk.Entry()
         self.entry.set_text("Hello World")
-	
-	self.hbox = gtk.HButtonBox()
-	self.hbox.set_layout(gtk.BUTTONBOX_SPREAD)
-	self.hbox.set_border_width(5)
-	
-	self.bbox = gtk.VButtonBox()
-	self.bbox.set_layout(gtk.BUTTONBOX_EDGE)
-	self.bbox.set_border_width(5)
-        self.bbox.pack_start(self.entry, True, True, 0)	
-	
-	self.button1 = gtk.Button('Generate graph!')
-	self.bbox.add(self.button1)
+    
+        #self.hbox = gtk.HButtonBox()
+        #self.hbox.set_layout(gtk.BUTTONBOX_SPREAD)
+        #self.hbox.set_border_width(5)
+        
+        #self.bbox = gtk.VButtonBox()
+        #self.bbox.set_layout(gtk.BUTTONBOX_EDGE)
+        #self.bbox.set_border_width(5)
+        #self.bbox.pack_start(self.entry, True, True, 0)    
+    
+        self.button1 = gtk.Button('Generate graph!')
+        self.bbox.add(self.button1)
 
-	self.button2 = gtk.Button('Save graph')
-	self.bbox.add(self.button2)
+        self.button2 = gtk.Button('Save graph')
+        self.bbox.add(self.button2)
 
-	self.button3 = gtk.Button('Compare two graphs')
-	self.bbox.add(self.button3)
+        self.button3 = gtk.Button('Compare two graphs')
+        self.bbox.add(self.button3)
 
         self.window.connect("delete_event", self.delete_event)
     
@@ -51,9 +55,12 @@ class GUI:
     
         self.button1.connect("clicked", self.generate_graph, None)
     
-        self.window.add(self.bbox)
+        #self.window.add(self.bbox)
     
-    	self.window.show_all()
+        grid.add(self.entry)
+        grid.attach_next_to(self.button1, self.entry, Gtk.PositionType.RIGHT, 2, 1)
+
+        self.window.show_all()
 
     def main(self):
         gtk.main()
