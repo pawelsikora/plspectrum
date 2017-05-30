f0b1f83344038827c7097514bf84fa0352cb1ffb
Pawel Sikora <sikor6@gmail.com>
2017-05-30 18:24:45 +0200

    Cleaning up commented code in gui.py

--- gui.py
+++ gui.py
@@ -6,21 +6,9 @@ from QSpectrum import Spectrum_generator
 
 class GUI:
 
-    # This is a callback function. The data arguments are ignored
-    # in this example. More on callbacks below.
     def generate_graph(self, widget, data=None):
         c = Spectrum_generator()
 	c.calculate_all()
-	
-	#print "Prawd"
-	#print c.params.Prawd
-	#print "LAMBDA"
-	#print c.params.LAMBDA
-	#print "Widmo"
-	#print c.params.Widmo
-	#print "Widmo5"
-	#print c.Widmo5
-
 	c.plot_widmo_beta()
 
     def delete_event(self, widget, event, data=None):
@@ -61,18 +49,11 @@ class GUI:
     
         self.window.set_border_width(30)
     
-        #self.button = gtk.Button("Generate graph!")
-    
         self.button1.connect("clicked", self.generate_graph, None)
     
-        #self.window.add(self.button)
         self.window.add(self.bbox)
-        #self.window.add(self.hbox)
-        #self.window.add(self.entry)
     
-        #self.button.show()
     	self.window.show_all()
-        #self.window.show()
 
     def main(self):
         gtk.main()
