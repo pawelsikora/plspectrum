a6694adadcebb8bc6688b34ead7168c3e58a24ba
Pawel Sikora <sikor6@gmail.com>
2017-06-21 12:10:51 +0200

    Add combobox to gui for choosing spectrum. Update QSpectrum.py - changing formatting to unix

--- QSpectrum.py
+++ QSpectrum.py
@@ -2,57 +2,58 @@
 
 from common import *
 from gui import *
-
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.font_manager import FontProperties
 
 __author__ = "Piotr Ruszala"
 __version__ = "0.1"
 
-        
+
 class Spectrum_generator:
 
     def __init__(self):
-        self.DOS = 0 
+        self.DOS = 0
         self.DOS2 = 0
-        self.Hevisajd = 0 
+        self.Hevisajd = 0
         self.Pik = 0
         self.Normal = 0
         self.Pik_mieszany = 0
         self.Delty= 0
         self.params = Parameters()
         self.params.update()
-        
-    def delta_cal(self):    
+
+    def delta_cal(self):
         for i in range(0, len(self.params.En)):
             self.DOS = self.DOS + self.params.g0 * self.params.CP[i] * \
                 ( DOS_rozmyty_erf(self.params.E, self.params.Eg + \
                  self.params.En[i], self.params.gamma_schodek) )
-            
+
             self.DOS2 = self.DOS2 + self.params.g0 * self.params.CP[i] * \
                 ( DOS_rozmyty_cauchy(self.params.E, self.params.Eg + \
                  self.params.En[i], self.params.gamma_schodek) )
-            
+
             self.Hevisajd = self.Hevisajd + self.params.g0 * \
                 self.params.CP[i] * \
                 Schodek(self.params.E-self.params.Eg - self.params.En[i])
-            
+
             self.Pik = self.Pik + self.params.A0 * \
                 ( pik_Lorentza(self.params.E, self.params.Eg + \
                  self.params.En[i], self.params.gamma) )
-            
+
             self.Normal = self.Normal + self.params.A0 * \
                 pik_Gaussowski(self.params.E, self.params.Eg + \
                 self.params.En[i], self.params.gamma)
-            
+
             self.Pik_mieszany = self.Pik_mieszany + self.params.A0 * \
                 ( pik_Lorentza(self.params.E, self.params.Eg + \
                   self.params.En[i], self.params.gamma) + \
                   pik_Gaussowski(self.params.E, self.params.Eg + \
                   self.params.En[i], self.params.gamma) )
-            
+
             self.Delty = self.Delty + \
                 self.params.A0 * \
                 Delta(self.params.E - self.params.Eg - self.params.En[i])
@@ -68,16 +69,16 @@ class Spectrum_generator:
     def widmo_fd(self):
         self.Widmo_fd = self.Pik * self.DOS * \
             distribution_FD(self.params.E, self.params.Ef, self.params.T)
-    
+
     def widmo_boltzmann(self):
         self.Widmo_b = self.Pik * self.DOS * \
             distribution_Boltzmann(0.0, self.params.E, self.params.T)
 
     def widmo_planck(self):
-        #Widmo PL QW ale obliczone sposobem jak dla Bulku (3D) - 
+        #Widmo PL QW ale obliczone sposobem jak dla Bulku (3D) -
         #absorpcja jedynie zmieniona na dwuwymiarowa! Przypadek rownowagi termodynamicznej!
         self.Widmo5 = (1.0 / self.params.E) * self.DOS * \
-        distribution_Planck(self.params.E, self.params.T) * self.params.E 
+        distribution_Planck(self.params.E, self.params.T) * self.params.E
 
     def prawdopodobienstwo(self):
         for j in range(0, len(self.params.En)):
@@ -86,28 +87,28 @@ class Spectrum_generator:
              Calka( self.params.E, pik_Gaussowski(self.params.E, \
                  self.params.E[i], self.params.gamma) * self.DOS * \
                 distribution_Boltzmann(0.0, self.params.E, self.params.T) )
-             
+
     def generate(self, items):
         for i in range(items):
             yield i
-    
+
     def widmo(self):
         self.params.Widmo = self.params.Prawd * self.params.E
-    
-    def save_to_files(self):
-        plik = open('widmo1', 'w')
-        plik2 = open('lambda', 'w')
-        
+
+    def save_to_files(self, plik, plik2):
+        self.plik = open(plik, 'w')
+        self.plik2 = open(plik2, 'w')
+
         for i in self.generate(len(self.params.Widmo)):
-            plik.write(str(self.params.LAMBDA[i]) + ' ' \
+            self.plik.write(str(self.params.LAMBDA[i]) + ' ' \
                        + str(self.params.Widmo[i]/max(self.params.Widmo)) \
                        + '\n')
-        
+
         for i in self.generate(len(self.params.LAMBDA)):
-            plik2.write(str(self.params.LAMBDA[i]) + '\n')
-        
-        plik2.close()
-        plik.close()
+            self.plik2.write(str(self.params.LAMBDA[i]) + '\n')
+
+        self.plik2.close()
+        self.plik.close()
 
     def calculate_all(self):
         self.delta_cal()
@@ -117,16 +118,18 @@ class Spectrum_generator:
         self.widmo_planck()
         self.prawdopodobienstwo()
         self.widmo()
-        self.save_to_files()
+#self.save_to_files()
 
     def plot_widmo_alfa(self):
-        plt.plot(self.params.E, self.DOS/max(self.DOS), 'r', self.params.E, \
-             self.DOS2/max(self.DOS2), 'b', self.params.E, \
-             self.Hevisajd/max(self.Hevisajd), 'g', lw=2)
+        one = plt.plot(self.params.E, self.DOS/max(self.DOS), 'r', label="one")
+        two = plt.plot(self.params.E, self.DOS2/max(self.DOS2), 'b', label="two")
+        three = plt.plot(self.params.E, self.Hevisajd/max(self.Hevisajd), 'g', lw=2, label="three")
 
         plt.savefig("plot_alfa.png")
+        self.generated_alfa = gtk.Image.new_from_file("plot_alfa.png")
+
         plt.show()
-    
+
     def plot_widmo_beta(self):
         self.count = len(open('LAMBDA_Zmierzona.txt', 'rU').readlines())
         self.zmierzone_x = open('LAMBDA_Zmierzona.txt', 'rU').readlines()
@@ -135,14 +138,15 @@ class Spectrum_generator:
         for i in range(0, self.count):
             self.zmierzone_x[i] = float(self.zmierzone_x[i].strip())
             self.zmierzone_y[i] = float(self.zmierzone_y[i].strip())
-        
+
         plt.plot(self.params.LAMBDA, self.Widmo5/max(self.Widmo5), 'r', \
              self.params.LAMBDA, self.params.Widmo/max(self.params.Widmo), \
              'b', self.zmierzone_x, self.zmierzone_y, 'k.', lw=2)
 
         plt.savefig("plot_beta.png")
+        self.generated_beta = gtk.Image.new_from_file("plot_beta.png")
         plt.show()
-        
+
 
 if __name__ == "__main__":
 
--- gui.py
+++ gui.py
@@ -1,26 +1,92 @@
 from gi import pygtkcompat
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_gtk3cairo import FigureCanvasGTK3Cairo as FigureCanvas
 
-pygtkcompat.enable() 
+pygtkcompat.enable()
 pygtkcompat.enable_gtk(version='3.0')
 
 import gtk
 
 from QSpectrum import Spectrum_generator
+class DialogExample(gtk.Dialog):
+
+    def __init__(self, parent):
+        gtk.Dialog.__init__(self, "My Dialog", parent, 0,
+            (gtk.STOCK_CANCEL, gtk.ResponseType.CANCEL,
+             gtk.STOCK_OK, gtk.ResponseType.OK))
+
+        self.set_default_size(150, 100)
+
+        label = gtk.Label("This is a dialog to display additional information")
+
+        box = self.get_content_area()
+        box.add(label)
+        self.show_all()
 
 class GUI:
 
     def generate_graph(self, widget, data=None):
-        c = Spectrum_generator()
 
-        c.arams.A0 = float(self.entry_param_a0.get_text())
-        c.params.g0 = float(self.entry_param_g0.get_text())
-        c.params.Eg = float(self.entry_param_eg.get_text())
-        c.params.T = float(self.entry_param_T.get_text())
-        c.params.gamma = float(self.entry_param_gamma.get_text())
-        c.params.gamma_schodek = float(self.entry_param_gamma_schodek.get_text())
+        self.c = Spectrum_generator()
+        self.c.params.A0 = float(self.entry_param_a0.get_text())
+        self.c.params.g0 = float(self.entry_param_g0.get_text())
+        self.c.params.Eg = float(self.entry_param_eg.get_text())
+        self.c.params.T = float(self.entry_param_T.get_text())
+        self.c.params.gamma = float(self.entry_param_gamma.get_text())
+        self.c.params.gamma_schodek = float(self.entry_param_gamma_schodek.get_text())
+        if (self.c.params.A0) == 0 or self.c.params.g0  == 0 or \
+           self.c.params.Eg == 0 or self.c.params.T == 0 or \
+           self.c.params.gamma == 0 or self.c.params.gamma_schodek == 0:
+              print("some value is 0!")
+              dialog1 = gtk.MessageDialog(None, 0, gtk.MessageType.INFO,
+                  gtk.ButtonsType.OK, "Values can't be zero!")
+              dialog1.format_secondary_text(
+                  "some of your values appears to be empty. Please check it and type again")
+              dialog1.run()
+              dialog1.destroy()
 
-        c.calculate_all()
-        c.plot_widmo_beta()
+        self.spectrum_choice = self.spectrum_combobox.get_active()
+
+        if self.spectrum_choice == 1:
+           self.frame1.remove(self.currentGraph)
+           self.c.calculate_all()
+           self.c.plot_widmo_alfa()
+           self.currentGraph = self.c.generated_alfa
+        elif self.spectrum_choice == 2:
+           self.frame1.remove(self.currentGraph)
+           self.c = Spectrum_generator()
+           self.c.calculate_all()
+           self.c.plot_widmo_beta()
+           self.currentGraph = self.c.generated_beta
+        else:
+           dialog = gtk.MessageDialog(None, 0, gtk.MessageType.INFO,
+               gtk.ButtonsType.OK, "Wrong choice!")
+           dialog.format_secondary_text(
+               "You have not chosen any spectrum to generate. Please choose one and try again")
+           dialog.run()
+           dialog.destroy()
+
+        self.frame1.add(self.currentGraph)
+        self.window.show_all()
+
+    def save_to_origin(self, widget, data=None):
+        self.spectrum_choice = self.spectrum_combobox.get_active()
+        if self.spectrum_choice == 1:
+           self.c.save_to_files("widmo_alfa", "lambda_alfa")
+        elif self.spectrum_choice == 2:
+           self.c.save_to_files("widmo_beta", "lambda_beta")
+        dialog3 = gtk.FileChooserDialog("Please choose a file", None,
+            gtk.FileChooserAction.SAVE,
+            (gtk.STOCK_CANCEL, gtk.ResponseType.CANCEL,
+             gtk.STOCK_OPEN, gtk.ResponseType.OK))
+
+        response = dialog3.run()
+        if response == gtk.ResponseType.OK:
+            print("Open clicked")
+            print("File selected: " + dialog3.get_filename())
+
+        dialog3.destroy()
+
 
     def delete_event(self, widget, event, data=None):
         print("delete event occurred")
@@ -30,19 +96,49 @@ class GUI:
         print("destroy signal occurred")
         gtk.main_quit()
 
+    def on_spectrum_combo_changed(self, combo):
+        tree_iter = combo.get_active_iter()
+        if tree_iter != None:
+            model = combo.get_model()
+            row_id, name = model[tree_iter][:2]
+            print("Selected: ID=%d, name=%s" % (row_id, name))
+        else:
+            entry = combo.get_child()
+            print("Entered: %s" % entry.get_text())
+
     def __init__(self):
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        self.window.set_size_request(500, 350)    
-    
-        grid = gtk.Grid()
-        self.window.add(grid)    
+        self.window.set_size_request(1024, 450)
+        self.window.set_icon_from_file('icon.png')
+        self.grid = gtk.Grid()
 
+        self.grid.set_column_spacing(10)
+        self.grid.set_row_spacing(10)
+        self.window.add(self.grid)
+
+        self.initGraph = gtk.Image.new_from_file('plot_alfa.png')
+        self.currentGraph = self.initGraph
+
+        spectrum_store = gtk.ListStore(int, str)
+        spectrum_store.append([1, "--- Choose spectrum ---"])
+        spectrum_store.append([11, "Widmo alfa"])
+        spectrum_store.append([12, "Widmo beta"])
+        spectrum_store.append([2, "Widmo gamma"])
+        self.spectrum_combobox = gtk.ComboBox.new_with_model_and_entry(spectrum_store)
+        self.spectrum_combobox.connect("changed", self.on_spectrum_combo_changed)
+        self.spectrum_combobox.set_entry_text_column(1)
+        self.spectrum_combobox.set_active(0)
+
+        self.frame1 = gtk.Frame(label="Graph")
+        self.frame1.set_label_align(0.5, 0.5)
+        self.frame1.add(self.currentGraph)
         self.label_param_a0 = gtk.Label("A0")
         self.label_param_g0 = gtk.Label("g0")
         self.label_param_eg = gtk.Label("Eg")
         self.label_param_T = gtk.Label("T")
         self.label_param_gamma = gtk.Label("Gamma")
         self.label_param_gamma_schodek = gtk.Label("Gamma schodek")
+        self.label_param_meh = gtk.Label("meh")
 
         self.label_param_a0.set_xalign(1)
         self.label_param_g0.set_xalign(1)
@@ -50,6 +146,7 @@ class GUI:
         self.label_param_T.set_xalign(1)
         self.label_param_gamma.set_xalign(1)
         self.label_param_gamma_schodek.set_xalign(1)
+        self.label_param_meh.set_xalign(1)
 
         self.entry_param_a0 = gtk.Entry()
         self.entry_param_g0 = gtk.Entry()
@@ -57,35 +154,48 @@ class GUI:
         self.entry_param_T = gtk.Entry()
         self.entry_param_gamma = gtk.Entry()
         self.entry_param_gamma_schodek = gtk.Entry()
+        self.entry_param_meh = gtk.Entry()
 
         self.button1 = gtk.Button('Generate graph!')
+        self.buttonExportToOrigin = gtk.Button('Export data to txt')
+        self.buttonExportToOrigin.connect("clicked", self.save_to_origin, None)
+        self.buttonSaveImage = gtk.Button('Save Graph')
+
+        self.button5 = gtk.Button()
 
         self.button2 = gtk.Button('Save graph')
 
         self.button3 = gtk.Button('Compare two graphs')
 
         self.window.connect("delete_event", self.delete_event)
-    
+
         self.window.connect("destroy", self.destroy)
-    
+
         self.window.set_border_width(30)
-    
+
         self.button1.connect("clicked", self.generate_graph, None)
 
-        grid.add(self.label_param_a0)
-        grid.attach_next_to(self.label_param_g0, self.label_param_a0, gtk.PositionType.BOTTOM, 1, 2)
-        grid.attach_next_to(self.label_param_eg, self.label_param_g0, gtk.PositionType.BOTTOM, 1, 3)
-        grid.attach_next_to(self.label_param_T, self.label_param_eg, gtk.PositionType.BOTTOM, 1, 4)
-        grid.attach_next_to(self.label_param_gamma, self.label_param_T, gtk.PositionType.BOTTOM, 1, 5)
-        grid.attach_next_to(self.label_param_gamma_schodek, self.label_param_gamma, gtk.PositionType.BOTTOM, 1, 6)
+        self.grid.attach(self.label_param_a0, 0, 2, 1, 1)
+        self.grid.attach_next_to(self.label_param_g0, self.label_param_a0, gtk.PositionType.BOTTOM, 1, 1)
+        self.grid.attach_next_to(self.label_param_eg, self.label_param_g0, gtk.PositionType.BOTTOM, 1, 1)
+        self.grid.attach_next_to(self.label_param_T, self.label_param_eg, gtk.PositionType.BOTTOM, 1, 1)
+        self.grid.attach_next_to(self.label_param_gamma, self.label_param_T, gtk.PositionType.BOTTOM, 1, 1)
+        self.grid.attach_next_to(self.label_param_gamma_schodek, self.label_param_gamma, gtk.PositionType.BOTTOM, 1, 1)
+        self.grid.attach_next_to(self.label_param_meh, self.label_param_gamma_schodek, gtk.PositionType.BOTTOM, 1, 1)
 
-        grid.add(self.entry_param_a0)
-        grid.attach_next_to(self.entry_param_g0, self.entry_param_a0, gtk.PositionType.BOTTOM, 2, 2)
-        grid.attach_next_to(self.entry_param_eg, self.entry_param_g0, gtk.PositionType.BOTTOM, 2, 3)
-        grid.attach_next_to(self.entry_param_T, self.entry_param_eg, gtk.PositionType.BOTTOM, 2, 4)
-        grid.attach_next_to(self.entry_param_gamma, self.entry_param_T, gtk.PositionType.BOTTOM, 2, 5)
-        grid.attach_next_to(self.entry_param_gamma_schodek, self.entry_param_gamma, gtk.PositionType.BOTTOM, 2, 6)
-        grid.attach_next_to(self.button1, self.entry_param_gamma_schodek, gtk.PositionType.RIGHT, 3, 6)
+        self.grid.attach(self.entry_param_a0, 1, 2, 1, 1)
+        self.grid.attach_next_to(self.entry_param_g0, self.entry_param_a0, gtk.PositionType.BOTTOM, 1, 1)
+        self.grid.attach_next_to(self.entry_param_eg, self.entry_param_g0, gtk.PositionType.BOTTOM, 1, 1)
+        self.grid.attach_next_to(self.entry_param_T, self.entry_param_eg, gtk.PositionType.BOTTOM, 1, 1)
+        self.grid.attach_next_to(self.entry_param_gamma, self.entry_param_T, gtk.PositionType.BOTTOM, 1, 1)
+        self.grid.attach_next_to(self.entry_param_gamma_schodek, self.entry_param_gamma, gtk.PositionType.BOTTOM, 1, 1)
+        self.grid.attach_next_to(self.entry_param_meh, self.entry_param_gamma_schodek, gtk.PositionType.BOTTOM, 1, 1)
+
+        self.grid.attach(self.frame1, 3, 0, 6, 14)
+        self.grid.attach(self.button1, 6, 15, 3, 1)
+        self.grid.attach_next_to(self.buttonExportToOrigin, self.button1, gtk.PositionType.TOP, 1, 1)
+        self.grid.attach_next_to(self.buttonSaveImage, self.buttonExportToOrigin, gtk.PositionType.RIGHT, 1, 1)
+        self.grid.attach_next_to(self.spectrum_combobox, self.buttonExportToOrigin, gtk.PositionType.LEFT, 1, 1)
 
         self.window.show_all()
 
