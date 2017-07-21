9ca7cad326574731256595b465d46b029715f887
Pawel Sikora <sikor6@gmail.com>
2017-07-21 12:12:54 +0200

    main: remove Eg from calculations, change some variables' names

--- QSpectrum.py
+++ QSpectrum.py
@@ -14,17 +14,18 @@ __version__ = "0.1"
 class Spectrum_generator:
 
     def __init__(self, file_with_measured_data="None"):
-        self.DOS = 0
-        self.DOS2 = 0
-        self.JDOS = 0
-        self.JDOS2 = 0
-        self.dosC = 0
-        self.dosV = 0
-        self.Hevisajd = 0
-        self.Pik = 0
-        self.Normal = 0
-        self.Pik_mieszany = 0
-        self.Delty= 0
+        self.DOS = np.zeros(len(self.params.E))
+        self.DOS2 = np.zeros(len(self.params.E))
+        self.JDOS = np.zeros(len(self.params.E))
+        self.JDOS2 = np.zeros(len(self.params.E))
+        self.dosC = np.zeros(len(self.params.Ec))
+        self.dosV = np.zeros(len(self.params.Ev))
+        self.Hevisajd = np.zeros(len(self.params.E))
+        self.Pik = np.zeros(len(self.params.E))
+        self.Normal = np.zeros(len(self.params.E))
+        self.Pik_mieszany = np.zeros(len(self.params.E))
+        self.Delty = np.zeros(len(self.params.E))
+
         self.params = Parameters()
         self.params.update()
         self.f_measured_data = file_with_measured_data
@@ -32,34 +33,34 @@ class Spectrum_generator:
     def delta_cal(self):
         for i in range(0, len(self.params.En)):
             self.DOS = self.DOS + self.params.g0 * self.params.CP[i] * \
-                ( DOS_rozmyty_erf(self.params.E, self.params.Eg + \
+                (DOS_rozmyty_erf(self.params.E, \
                  self.params.En[i], self.params.gamma_schodek) )
 
             self.DOS2 = self.DOS2 + self.params.g0 * self.params.CP[i] * \
-                ( DOS_rozmyty_cauchy(self.params.E, self.params.Eg + \
+                ( DOS_rozmyty_cauchy(self.params.E, \
                  self.params.En[i], self.params.gamma_schodek) )
 
             self.Hevisajd = self.Hevisajd + self.params.g0 * \
                 self.params.CP[i] * \
-                Schodek(self.params.E-self.params.Eg - self.params.En[i])
+                Schodek(self.params.E - self.params.En[i])
 
             self.Pik = self.Pik + self.params.A0 * \
-                ( pik_Lorentza(self.params.E, self.params.Eg + \
+                ( pik_Lorentza(self.params.E, \
                  self.params.En[i], self.params.gamma) )
 
             self.Normal = self.Normal + self.params.A0 * \
-                pik_Gaussowski(self.params.E, self.params.Eg + \
+                pik_Gaussowski(self.params.E, \
                 self.params.En[i], self.params.gamma)
 
             self.Pik_mieszany = self.Pik_mieszany + self.params.A0 * \
-                ( pik_Lorentza(self.params.E, self.params.Eg + \
+                ( pik_Lorentza(self.params.E, \
                   self.params.En[i], self.params.gamma) + \
-                  pik_Gaussowski(self.params.E, self.params.Eg + \
+                  pik_Gaussowski(self.params.E, \
                   self.params.En[i], self.params.gamma) )
 
             self.Delty = self.Delty + \
                 self.params.A0 * \
-                Delta(self.params.E - self.params.Eg - self.params.En[i])
+                Delta(self.params.E - self.params.En[i])
 
             self.dosC = self.dosC + self.params.g0 * self.params.me * \
                 (Schodek(self.params.Ec - self.params.CB[i]))
@@ -68,11 +69,11 @@ class Spectrum_generator:
                 (Schodek(self.params.Ev - self.params.VB[i]))
 
             self.JDOS = self.JDOS + self.params.g0 * \
-                (DOS_rozmyty_erf(self.params.E, self.params.Eg + self.params.En[i], \
+                (DOS_rozmyty_erf(self.params.E, self.params.En[i], \
                                  self.params.gamma_schodek))
 
             self.JDOS2 = self.JDOS2 + self.params.g0 * \
-                (DOS_rozmyty_cauchy(self.params.E, self.params.Eg + self.params.En[i], \
+                (DOS_rozmyty_cauchy(self.params.E, self.params.En[i], \
                                     self.params.gamma_schodek))
 
     def energia_pocz(self):
@@ -91,32 +92,30 @@ class Spectrum_generator:
             distribution_Boltzmann(0.0, self.params.E, self.params.T)
 
     def widmo_planck(self):
-        self.Widmo5 = (1.0 / self.params.E) * self.DOS * \
+        self.Widmo1 = (1.0 / self.params.E) * self.DOS * \
         distribution_Planck(self.params.E, self.params.T) * self.params.E
 
     def prawdopodobienstwo(self):
         self.Prawd = np.zeros(len(self.params.E))
-        for j in range(0, len(self.params.En)):
-            for i in range(0, len(self.params.E)):
-             self.Prawd[i] = self.Prawd[i] + \
-             Calka( self.params.E, pik_Gaussowski(self.params.E, \
-                 self.params.E[i], self.params.gamma) * self.DOS * \
-                distribution_Boltzmann(0.0, self.params.E, self.params.T) )
+        for j in range(0, len(self.params.E)):
+             self.Prawd[i] = Calka( self.params.E, pik_Gaussowski(self.params.E, \
+                             self.params.E[i], self.params.gamma) * self.DOS * \
+                             distribution_Boltzmann(0.0, self.params.E, self.params.T) )
 
     def generate(self, items):
         for i in range(items):
             yield i
 
     def widmo(self):
-        self.Widmo = self.Prawd * self.params.E
+        self.Widmo2 = self.Prawd * self.params.E
 
     def save_to_files(self, plik, plik2):
         self.plik = open(plik, 'w')
         self.plik2 = open(plik2, 'w')
 
-        for i in self.generate(len(self.Widmo)):
+        for i in self.generate(len(self.Widmo2)):
             self.plik.write(str(self.params.LAMBDA[i]) + ' ' \
-                       + str(self.Widmo[i]/max(self.Widmo)) \
+                       + str(self.Widmo2[i]/max(self.Widmo2)) \
                        + '\n')
 
         for i in self.generate(len(self.params.LAMBDA)):
@@ -156,18 +155,18 @@ class Spectrum_generator:
                                         usecols=(0,1), unpack=True)
             print(self.x)
             print(self.y)
-            print(self.Widmo)
+            print(self.Widmo2)
             print("X and Y updated!")
-            data_plt = plt.plot(self.params.LAMBDA, self.Widmo5/max(self.Widmo5), 'r', \
-                 self.params.LAMBDA, self.Widmo/max(self.Widmo), \
+            data_plt = plt.plot(self.params.LAMBDA, self.Widmo1/max(self.Widmo1), 'r', \
+                 self.params.LAMBDA, self.Widmo2/max(self.Widmo2), \
                  'b', self.x, self.y, 'k.', lw=2)
             self.linex = data_plt[0].get_data()
             self.liney = data_plt[1].get_data()
             self.linez = data_plt[2].get_data()
         else:
             print("X/Y NOT updated!")
-            data_plt = plt.plot(self.params.LAMBDA, self.Widmo5/max(self.Widmo5), 'r', \
-                 self.params.LAMBDA, self.Widmo/max(self.Widmo), \
+            data_plt = plt.plot(self.params.LAMBDA, self.Widmo1/max(self.Widmo1), 'r', \
+                 self.params.LAMBDA, self.Widmo2/max(self.Widmo2), \
                  'b', lw=2)    
             self.linex = data_plt[0].get_data()
             self.liney = data_plt[1].get_data()
@@ -203,7 +202,7 @@ class Spectrum_generator:
     def plot_widmo_jdos(self):
         data_plt_j = plt.plot(self.params.E, self.JDOS/max(self.JDOS), 'r', \
                        self.params.E, self.JDOS2/max(self.JDOS2), 'b', \
-                       self.params.E, self.Hevisajd/max(self.Hevisajd), lw=2)
+                       self.params.E, self.Hevisajd/max(self.Hevisajd), 'k', lw=2)
 
         self.linex = data_plt_j[0].get_data()
         self.liney = data_plt_j[1].get_data()
